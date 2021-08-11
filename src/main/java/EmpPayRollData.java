e34b7b2aea9583bb9202923dd8c0c05cfa0411d5
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-08-12 00:18:10 +0530

    Creating EmpPayRoll

---
+++ src/main/java/EmpPayRollData.java
@@ -0,0 +1,15 @@
+public class EmpPayRollData {
+        /*
+        Declaring Variables and Used Constructor
+         */
+        private int id;
+        private String name;
+        private double salary;
+
+        public EmpPayRollData(int id, String name, double salary) {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+    }
+
